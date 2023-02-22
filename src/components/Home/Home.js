71193e92d6965b7f3214109b0ed86237ef79cbef
dnKhanh22 <khanhdoan227@gmail.com>
2023-02-22 16:06:36 +0700

    update 1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                 </div>
                 <div className="home-wapper">
                     <h3>hey! i am</h3>
-                    <h2>Ronaldo Fredrickson</h2>
+                    <h2 className="">Ronaldo Fredrickson</h2>
                     <h2>I'm a <span>Front-end.; Back-end</span></h2>
                 </div>
             </div>
