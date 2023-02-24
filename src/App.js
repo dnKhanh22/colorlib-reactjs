1c7e84e82c175978652047ad56d14da722980a71
dnKhanh22 <khanhdoan227@gmail.com>
2023-02-24 14:39:38 +0700

    update 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header/Header';
 import About from './components/About/About';
 import Home from './components/Home/Home';
+import { BrowserRouter as Router, Switch, Route, Link, Routes } from 'react-router-dom';
+
 
 
 const App = () => {
@@ -10,7 +12,16 @@ const App = () => {
     <div>
       <Header />
       <Home />
-      <About />
+      {/* <Router>
+        <Header />
+          <Routes>
+            <Route exact path= '/home' element ={<Home/>} />
+            <Route exact path= '/about' element ={<About/>} />
+
+      
+          </Routes>
+
+        </Router> */}
     </div>
   )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,38 @@
-import React from "react";
+
+import React, { useState } from "react";
 import './styles/header.scss'
+import { Link } from "react-router-dom";
+
 
 const Header = () => {
+    const [active, setActive] = useState(false) 
+    console.log(active)
     return (
+        // <div className="navbar">
+        //     <div className="navbar-item">
+        //         <h1 className="nav-brand">Ronaldo</h1>
+
+        //         <ul className="nav-links">
+        //             <Link to='/home'><li>Home</li></Link>
+        //             <Link to='/about'><li>About</li></Link>
+        //             <Link to='/resume'><li>Resume</li></Link>
+        //             <Link to='/service'><li>Services</li></Link>
+        //             <Link to='/project'><li>Projects</li></Link>
+        //             <Link to='/blog'><li>My Blog</li></Link>
+        //             <Link to='/contact'><li>Contact</li></Link>
+                
+        //         </ul>
+        //     </div>
+            
+        // </div>
         <header>
             <div className="container">
                 <div className="navbar">
                     <div  className="navbar-brand">
-                        <h1>Ronaldo</h1>
+                        <h1><span className="nav-brand">R</span><span>onaldo</span></h1>
                     </div>
 
-                    <div className="navbar-actions">
+                    <div id="nav-act" className="navbar-actions">
                         <ul>
                             <li className="navbar-item"><a href="#">Home<div className="navbar-line"></div></a></li>
                             <li className="navbar-item"><a href="#">About<div className="navbar-line"></div></a></li>
@@ -23,6 +45,17 @@ const Header = () => {
                         </ul>
 
                     </div>
+                    <label className="navbar-icon" onClick={()=>setActive(!active)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        {/* <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                        <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+                    </label>
+                    {
+                        active ? (
+                            <div> </div>
+                        ) : null
+                    }
+                    
                 </div>
 
             </div>
